6264c9e15ca6e6c39c435bed10833c5baf1654ab
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2025-01-03 23:44:53 +0200

    Create Cancel.vue

---
+++ src/components/Cancel.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="cancel">
+    <h1>Plata a fost anulată.</h1>
+    <p>Încercați din nou dacă doriți să contribuiți.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cancel",
+};
+</script>
