4e7debe7e2acc69edf4a6796eeb9f643b058562b
AugmentedRealityweb <160763410+AugmentedRealityweb@users.noreply.github.com>
2025-01-03 23:43:17 +0200

    Create Success.vue

---
+++ src/components/Success.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="success">
+    <h1>Mulțumim pentru donație!</h1>
+    <p>Susținerea ta înseamnă mult pentru noi!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Success",
+};
+</script>
